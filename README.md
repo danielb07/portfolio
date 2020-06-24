204ff5e2532db3a00364d30a9e6503f7a58d54e0
danielb07 <danielbarraud07@gmail.com>
2020-06-24 22:14:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Your Project's Name
+Write your project's name and write what you have done here
---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+
+ 
+var engine;
+var world;
+var box
+ 
+var ground;
+var gSlider;
+
+ setup = () => {
+    // createCanvas(200,200);
+    // createCanvas(displayWidth,displayHeight);
+
+}
+ 
+ draw= () => {
+
+}
+// this will help with the position
+mousePressed = () => {
+    console.log(mouseX,", ",mouseY);
+    
+}
